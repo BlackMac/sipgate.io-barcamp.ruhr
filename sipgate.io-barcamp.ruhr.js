6c4267562ca43944866d16a4c35e21b6c54edb17
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-03-19 23:00:21 +0100

    adds incoming call response setting

--- sipgate.io-barcamp.ruhr.js
+++ sipgate.io-barcamp.ruhr.js
@@ -1,4 +1,5 @@
 calls = new Mongo.Collection("calls");
+config = new Mongo.Collection("config");
 
 if (Meteor.isClient) {
   Template.iocall.helpers({
@@ -21,6 +22,35 @@ if (Meteor.isClient) {
       return "ausgehender";
     }
   });
+
+  Template.ioconfig.events ({
+    "change #incoming_handling": function (event){
+      var inputValue = event.target.value;
+      var settings = config.findOne({});
+      config.remove({ _id:settings._id });
+      config.insert({
+        incoming:inputValue
+      });
+    }
+  });
+
+  Template.ioconfig.helpers({
+    "allowSelected": function() {
+      var settings = config.findOne({});
+      if (typeof settings ==="undefined") return "selected";
+      return (settings.incoming === "allow" ? "selected" : "");
+    },
+    "denySelected": function() {
+      var settings = config.findOne({});
+      if (typeof settings ==="undefined") return "";
+      return (settings.incoming === "deny" ? "selected" : "");
+    },
+    "busySelected": function() {
+      var settings = config.findOne({});
+      if (typeof settings ==="undefined") return "";
+      return (settings.incoming === "busy" ? "selected" : "");
+    }
+  });
 }
 
 if (Meteor.isServer) {
@@ -29,8 +59,26 @@ if (Meteor.isServer) {
       if (typeof data !== "undefined") {
         calls.remove({});
         calls.insert(parsePost(data.toString()));
-        return '<?xml version="1.0" encoding="UTF-8"?><Response></Response>';
+        var settings = config.findOne({});
+        this.setContentType('application/xml');
+        if (settings.incoming === "allow") {
+          return '<?xml version="1.0" encoding="UTF-8"?><Response></Response>';
+        } else if (settings.incoming === "busy") {
+          return '<?xml version="1.0" encoding="UTF-8"?><Response><Reject reason="busy"/></Response>';
+        } else {
+          return '<?xml version="1.0" encoding="UTF-8"?><Response><Reject /></Response>';
+        }
+      } else {
+        return "No Data";
       }
+    }
+  });
+  Meteor.startup(function () {
+    var settings = config.findOne({});
+    if (typeof settings === "undefined") {
+      config.insert({
+        incoming:"allow"
+      });
     }
   });
 }
