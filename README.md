fbf31e0e8abf11ae53d623ef144cf4303db81879
Stefan Lange-Hegermann <stefan@app-architect.com>
2015-03-19 21:18:52 +0100

    displays the single most recent call

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sipgate.io-barcamp.ruhr
+
+## requirements
+
+### remote access
+
+You need a way to expose your local machine to the public internet. You can also do this using port forwarding or an ssh reverse proxy. proxylocal is pretty much the easiest way though.
+
+    sudo gem install proxylocal
+
+### router
+
+To be able to respond differently to another URL you will need a router. A very simpe solution for meteor is the [cfs:http-methods package](https://github.com/CollectionFS/Meteor-http-methods).
--- sipgate.io-barcamp.ruhr.js
+++ sipgate.io-barcamp.ruhr.js
@@ -1,22 +1,27 @@
+calls = new Mongo.Collection("calls");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.iocall.helpers({
+    currentCall: function () {
+      return calls.findOne();
     }
   });
 }
 
 if (Meteor.isServer) {
+  HTTP.methods({
+    'io': function(data) {
+      if (typeof data !== "undefined") {
+        calls.remove({});
+        calls.insert(parsePost(data.toString()));
+        return '<?xml version="1.0" encoding="UTF-8"?><Response></Response>';
+      }
+    }
+  }),
+
   Meteor.startup(function () {
     // code to run on server at startup
   });
